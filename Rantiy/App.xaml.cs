4ad9ebe7d4f39528b6ed26416ba26f0ad6593700
Daniel Monettelli <danielmonetelli@hotmail.com>
2024-05-26 19:40:42 -0500

    🎉 [Code] Added initial .NET MAUI template

---
+++ Rantiy/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Rantiy
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Rantiy/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Rantiy
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
